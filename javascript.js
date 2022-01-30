044f734e05c6eec886bc4550d6118289aea19502
Cam <cej528@gmail.com>
2022-01-29 20:07:57 -0600

    modified javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,36 @@
-function playRound(playerSelection, computerSelection) {
-    //Code goes here
-}
+function game(x) {
+    function playRound(playerSelection, computerSelection) {
+        function playerSelection() {
+            let x = y
+        }
+
+        function computerSelection {
+            let options = ['Rock', 'Paper', 'Scissors'];
+            let choices = options[Math.floor(Math.random() * options.length)]
+            //have choices = z
+        }
+    }
+
+    function results {
+        if (condition1) {
+            //Tie
+        } else if (condition2) {
+            if (condition1) {
+                //You win
+            } else {
+                //You lose
+            }
+        } else if (condition2) {
+            if (condition1) {
+                //You win
+            } else {
+                //You lose
+            } 
+            else if (condition2) {
+                if (condition1) {
+                    //You win
+                } else {
+                    //You lose
+                }
+            }
+        }
