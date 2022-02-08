782ddbbaa53d89c70834c07a9f625fd62870bc51
Cam <cej528@gmail.com>
2022-02-07 22:29:21 -0600

    Modified javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 
-function computerSelection {
-        let options = ['Rock', 'Paper', 'Scissors']; // Fire = Rock, Water = Paper, Grass = Scissors
+function computerSelection () {
+        let options = ['Rock', 'Paper', 'Scissors'];
         let choices = options[Math.floor(Math.random() * options.length)];
-}
+        return choices;
+} //This function works properly, no further testing needed
 
 
 let playerSelection = ['Rock', 'Paper', 'Scissors'];
@@ -10,17 +11,17 @@ let playerSelection = ['Rock', 'Paper', 'Scissors'];
 function playRound(playerSelection, computerSelection) {
         if (playerSelection == computerSelection) {
             console.log('Tie!');
-        } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        } else if (playerSelection== 'Rock' && computerSelection == 'Scissors') {
             console.log('You win!');
-        } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
             console.log('You lose!');
-        } else if (playerSelection === 'Paper' && computerSelection == 'Rock') {
+        } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
             console.log('You win!');
-        } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
             console.log('You lose!');
-        } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
             console.log('You win!');
-        } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
             console.log('You lose!');
         }
     }
