6ffd5d73965d4cad39d2e2523a76bf46735ebe5c
Cam <cej528@gmail.com>
2022-02-08 21:07:47 -0600

    modified all files

--- javascript.js
+++ javascript.js
@@ -11,17 +11,35 @@ let playerSelection = ['Rock', 'Paper', 'Scissors'];
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         console.log('Tie!');
-    } else if (playerSelection== 'Rock' && computerSelection == 'Scissors') {
-        console.log('You win!');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        console.log('1 point for you!');
     } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        console.log('You lose!');
+        console.log('1 point for your opponent!');
     } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        console.log('You win!');
+        console.log('1 point for you!');
     } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        console.log('You lose!');
+        console.log('1 point for your opponent!');
     } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        console.log('You win!');
+        console.log('1 point for you!');
     } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-        console.log('You lose!');
+        console.log('1 point for your opponent!');
     }
 }
+
+const addScore = ['1 point for your opponent!', '1 point for you!', 'Tie!'];
+
+let myScore = 0;
+let theirScore = 0
+
+
+
+const btns = document.getElementById("btns");
+
+btns.addEventListener('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+
+  console.dir(event.target.id);
+})
