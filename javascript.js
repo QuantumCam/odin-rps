cd00330f4210e7e35877c71f6439d40e66ca5680
Cam <cej528@gmail.com>
2022-02-16 20:12:57 -0600

    modified index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -16,13 +16,11 @@ scissors.addEventListener('click', function(e) {
     console.log('Scissors');
 })
 
-
-
-function computerSelection () {
+function computerPlay () {
     let options = ['Rock', 'Paper', 'Scissors'];
     let choices = options[Math.floor(Math.random() * options.length)];
     return choices;
-} //This function works properly, no further testing needed
+}
 
 
 //I want to figure out how to have the HTML buttons connect with this declaration. 
@@ -52,3 +50,49 @@ function playRound(playerSelection, computerSelection) {
 
 let playerScore = 0;
 let computerScore = 0;
+
+
+/*
+//Game tracking with the DOM
+const container = document.querySelector('#btns');
+const resultsDiv = document.createElement('div');
+resultsDiv.style.marginTop = '50px'; //edited from 20px
+container.appendChild(resultsDiv);
+
+const playerWinText = document.createElement('p');
+playerWinText.style.color = 'blue';
+playerWinText.textContent = 'Player Win totals: ' + playerWin;
+resultsDiv.appendChild(playerWinText);
+
+const computerWinText = document.createElement('p');
+computerWinText.style.color = 'blue';
+computerWinText.textContent = 'Computer Win totals: ' + computerWin;
+resultsDiv.appendChild(computerWinText);
+
+const battleWinText = document.createElement('p'); //creates new P element
+battleWinText.style.color = 'black'; //changes color of new p element to black
+resultsDiv.appendChild(battleWinText);
+
+const gameWinText = document.createElement('p');
+gameWinText.style.color = 'green';
+gameWinText.textContent = gameWinner;
+resultsDiv.appendChild(gameWinText);
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameWinner = '';
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.className; //playerSelection is t
+            const computerSelection = computerPlay ();
+            battleWinText.textContent = (playRound(playerSelection, computerSelection));
+            playerWinText.textContent = 'Player Win totals: ' + playerWin;
+            compterWinText.textContent = 'Computer Win totals: ' + computerWin;
+            endGame ();
+        })
+    })
+
+*/
