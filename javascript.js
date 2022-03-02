a38e9bda09261b5b0daf8ad0e4b1341936ee78a0
Cam <cej528@gmail.com>
2022-03-02 13:38:42 -0600

    brought all files up to date

--- javascript.js
+++ javascript.js
@@ -1,47 +1,48 @@
+const fire = document.getElementById('fire'); //rock
+const grass = document.getElementById('grass'); //paper
+const water = document.getElementById('water'); //scissors
 
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
 
-
-rock.addEventListener('click', function(e) {
-    console.log('Rock');
+fire.addEventListener('click', function(e) {
+    console.log('fire');
 })
 
-paper.addEventListener('click', function(e) {
-    console.log('Paper');
+grass.addEventListener('click', function(e) {
+    console.log('grass');
 })
 
-scissors.addEventListener('click', function(e) {
-    console.log('Scissors');
+water.addEventListener('click', function(e) {
+    console.log('water');
 })
 
-function computerPlay () {
-    let options = ['Rock', 'Paper', 'Scissors'];
+
+
+function computerSelection () {
+    let options = ['fire', 'grass', 'water'];
     let choices = options[Math.floor(Math.random() * options.length)];
     return choices;
-}
+} //This function works properly, no further testing needed
 
 
 //I want to figure out how to have the HTML buttons connect with this declaration. 
- let playerSelection = ['Rock', 'Paper', 'Scissors'];
+ let playerSelection = ['fire', 'grass', 'water'];
 
 
  //Once I have the above problem figured out, we can have it go through this parameter.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         console.log('Tie!');
-    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    } else if (playerSelection == 'fire' && computerSelection == 'water') {
         console.log('1 point for you!');
-    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+    } else if (playerSelection == 'fire' && computerSelection == 'grass') {
         console.log('1 point for your opponent!');
-    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+    } else if (playerSelection == 'grass' && computerSelection == 'fire') {
         console.log('1 point for you!');
-    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+    } else if (playerSelection == 'grass' && computerSelection == 'water') {
         console.log('1 point for your opponent!');
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+    } else if (playerSelection == 'water' && computerSelection == 'grass') {
         console.log('1 point for you!');
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+    } else if (playerSelection == 'water' && computerSelection == 'fire') {
         console.log('1 point for your opponent!');
     }
 }
@@ -50,49 +51,3 @@ function playRound(playerSelection, computerSelection) {
 
 let playerScore = 0;
 let computerScore = 0;
-
-
-/*
-//Game tracking with the DOM
-const container = document.querySelector('#btns');
-const resultsDiv = document.createElement('div');
-resultsDiv.style.marginTop = '50px'; //edited from 20px
-container.appendChild(resultsDiv);
-
-const playerWinText = document.createElement('p');
-playerWinText.style.color = 'blue';
-playerWinText.textContent = 'Player Win totals: ' + playerWin;
-resultsDiv.appendChild(playerWinText);
-
-const computerWinText = document.createElement('p');
-computerWinText.style.color = 'blue';
-computerWinText.textContent = 'Computer Win totals: ' + computerWin;
-resultsDiv.appendChild(computerWinText);
-
-const battleWinText = document.createElement('p'); //creates new P element
-battleWinText.style.color = 'black'; //changes color of new p element to black
-resultsDiv.appendChild(battleWinText);
-
-const gameWinText = document.createElement('p');
-gameWinText.style.color = 'green';
-gameWinText.textContent = gameWinner;
-resultsDiv.appendChild(gameWinText);
-
-function game () {
-    let playerScore = 0;
-    let computerScore = 0;
-    let gameWinner = '';
-
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            playerSelection = button.className; //playerSelection is t
-            const computerSelection = computerPlay ();
-            battleWinText.textContent = (playRound(playerSelection, computerSelection));
-            playerWinText.textContent = 'Player Win totals: ' + playerWin;
-            compterWinText.textContent = 'Computer Win totals: ' + computerWin;
-            endGame ();
-        })
-    })
-
-*/
