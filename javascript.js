5ac59d733392bab9989f36b484517ca3ba16fb8c
Cam <cej528@gmail.com>
2022-03-09 11:48:05 -0600

    modified all files

--- javascript.js
+++ javascript.js
@@ -2,10 +2,17 @@ const fire = document.getElementById('fire'); //rock
 const grass = document.getElementById('grass'); //paper
 const water = document.getElementById('water'); //scissors
 
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('.pokemon-starters') 
+console.log(buttons);
+
 
 fire.addEventListener('click', function(e) {
     console.log('fire');
 })
+
 
 grass.addEventListener('click', function(e) {
     console.log('grass');
@@ -30,24 +37,29 @@ function computerSelection () {
 
  //Once I have the above problem figured out, we can have it go through this parameter.
 function playRound(playerSelection, computerSelection) {
+        let computerPick = computerSelection();
+        let results = '';
+
+
+
+
     if (playerSelection == computerSelection) {
-        console.log('Tie!');
+        results = ('Try again! You and your can\'t pick the same pokémon.');
     } else if (playerSelection == 'fire' && computerSelection == 'water') {
-        console.log('1 point for you!');
+        results = ('1 point for your rival! Water beats fire!');
     } else if (playerSelection == 'fire' && computerSelection == 'grass') {
-        console.log('1 point for your opponent!');
+        results = ('1 point for you! Fire beats grass!');
     } else if (playerSelection == 'grass' && computerSelection == 'fire') {
-        console.log('1 point for you!');
+        results = ('1 point for your rival! Fire beats grass!');
     } else if (playerSelection == 'grass' && computerSelection == 'water') {
-        console.log('1 point for your opponent!');
+        results = ('1 point for you! Grass beats water!');
     } else if (playerSelection == 'water' && computerSelection == 'grass') {
-        console.log('1 point for you!');
+        results = ('1 point for your rival! Grass beats water');
     } else if (playerSelection == 'water' && computerSelection == 'fire') {
-        console.log('1 point for your opponent!');
+        results = ('1 point for you! Water beats fire!');
     }
 }
 
 
 
-let playerScore = 0;
-let computerScore = 0;
+
