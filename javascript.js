b260fda2f8447b90a2d48f1952db3892f6bd50de
Cam <cej528@gmail.com>
2022-05-02 11:11:47 -0500

    modified all

--- javascript.js
+++ javascript.js
@@ -1,14 +1,7 @@
-/*
-const fire = document.getElementById('fire'); //rock
-const grass = document.getElementById('grass'); //paper
-const water = document.getElementById('water'); //scissors
-*/
-
-
 function myTest() {
     document.getElementById("test").innerHTML = "This is a test!";
 }
-
+//You will want to increment the score after each round
 let playerScore = 0;
 let computerScore = 0;
 
@@ -30,43 +23,36 @@ water.addEventListener('click', function(e) {
 })
 
 
+ // let playerSelection = ['fire', 'grass', 'water'];
 
-function computerSelection () {
+
+function computerPlay () {
     let options = ['fire', 'grass', 'water'];
     let choices = options[Math.floor(Math.random() * options.length)];
     return choices;
-} //This function works properly, no further testing needed
+}
 
-
-//I want to figure out how to have the HTML buttons connect with this declaration. 
- let playerSelection = ['fire', 'grass', 'water'];
-
-
- //Once I have the above problem figured out, we can have it go through this parameter.
 function playRound(playerSelection, computerSelection) {
-        let computerPick = computerSelection();
-        let results = '';
-
-
-
-
     if (playerSelection == computerSelection) {
-        results = ('Try again! You and your can\'t pick the same pokémon.');
+        return 'Try again! You and your can\'t pick the same pokémon.';
     } else if (playerSelection == 'fire' && computerSelection == 'water') {
-        results = ('1 point for your rival! Water beats fire!');
+        return '1 point for your rival! Water beats fire!';
     } else if (playerSelection == 'fire' && computerSelection == 'grass') {
-        results = ('1 point for you! Fire beats grass!');
+        return '1 point for you! Fire beats grass!';
     } else if (playerSelection == 'grass' && computerSelection == 'fire') {
-        results = ('1 point for your rival! Fire beats grass!');
+        return '1 point for your rival! Fire beats grass!';
     } else if (playerSelection == 'grass' && computerSelection == 'water') {
-        results = ('1 point for you! Grass beats water!');
+        return '1 point for you! Grass beats water!';
     } else if (playerSelection == 'water' && computerSelection == 'grass') {
-        results = ('1 point for your rival! Grass beats water');
+        return '1 point for your rival! Grass beats water';
     } else if (playerSelection == 'water' && computerSelection == 'fire') {
-        results = ('1 point for you! Water beats fire!');
+        return '1 point for you! Water beats fire!';
     }
 }
 
-
+let playerSelection = 'fire';
+let computerSelection = computerPlay();
+const areEqual = playerSelection.toLowerCase() === computerSelection.toLowerCase(); //Should make them case insensitive
+console.log(areEqual);
 
 
