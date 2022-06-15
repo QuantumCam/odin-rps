a058668dcab977b3b2426c6b7f5951ac1a186f51
Cam <cej528@gmail.com>
2022-06-15 15:05:10 -0500

    changed js to match main branch

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
-const fire = document.getElementById('fire'); //rock
-const grass = document.getElementById('grass'); //paper
-const water = document.getElementById('water'); //scissors
+function myTest() {
+    document.getElementById("test").innerHTML = "This is a test!";
+}
+//You will want to increment the score after each round
+let playerScore = 0;
+let computerScore = 0;
+
 
 
 fire.addEventListener('click', function(e) {
     console.log('fire');
 })
+
 
 grass.addEventListener('click', function(e) {
     console.log('grass');
@@ -16,38 +21,53 @@ water.addEventListener('click', function(e) {
 })
 
 
+const fireTest = document.getElementById('fire');
+fireTest.addEventListener('click', myFire);
+function myFire() {
+    document.getElementById('fireDemo').innerHTML = 'You chose Charmander, a fire-type pokémon!';
+}
 
-function computerSelection () {
+const waterTest = document.getElementById('water');
+waterTest.addEventListener('click', myWater);
+function myWater() {
+    document.getElementById('waterDemo').innerHTML = 'You chose Squirtle, a water-type pokémon!';
+}
+
+const grassTest = document.getElementById('grass');
+grassTest.addEventListener('click', myGrass);
+function myGrass() {
+    document.getElementById('grassDemo').innerHTML = 'You chose Bulbasaur, a grass-type pokémon!';
+}
+
+ // let playerSelection = ['fire', 'grass', 'water'];
+
+
+function computerPlay () {
     let options = ['fire', 'grass', 'water'];
     let choices = options[Math.floor(Math.random() * options.length)];
     return choices;
-} //This function works properly, no further testing needed
+}
 
+const computerSelection = computerPlay();
 
-//I want to figure out how to have the HTML buttons connect with this declaration. 
- let playerSelection = ['fire', 'grass', 'water'];
-
-
- //Once I have the above problem figured out, we can have it go through this parameter.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        console.log('Tie!');
+        return 'Try again! You and your can\'t pick the same pokémon.';
     } else if (playerSelection == 'fire' && computerSelection == 'water') {
-        console.log('1 point for you!');
+        return '1 point for your rival! Water beats fire!';
     } else if (playerSelection == 'fire' && computerSelection == 'grass') {
-        console.log('1 point for your opponent!');
+        return '1 point for you! Fire beats grass!';
     } else if (playerSelection == 'grass' && computerSelection == 'fire') {
-        console.log('1 point for you!');
+        return '1 point for your rival! Fire beats grass!';
     } else if (playerSelection == 'grass' && computerSelection == 'water') {
-        console.log('1 point for your opponent!');
+        return '1 point for you! Grass beats water!';
     } else if (playerSelection == 'water' && computerSelection == 'grass') {
-        console.log('1 point for you!');
+        return '1 point for your rival! Grass beats water';
     } else if (playerSelection == 'water' && computerSelection == 'fire') {
-        console.log('1 point for your opponent!');
+        return '1 point for you! Water beats fire!';
     }
 }
 
+/* const playerSelection =
 
-
-let playerScore = 0;
-let computerScore = 0;
+*/
